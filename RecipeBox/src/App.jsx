2d554fe41bdb4bd5b95c21e7a8b32724f30c785e
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-09-21 07:32:50 +0530

    Another custom hook, useOnline

--- RecipeBox/src/App.jsx
+++ RecipeBox/src/App.jsx
@@ -1,8 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
+import { useOnline } from './utils/useOnline';
 
 export default function App() {
+  const isOnline = useOnline();
+
+  if (!isOnline) {
+    return <h3 className='offline'>🥵 Offline!, please check your internet connection</h3>;
+  }
+
   return (
     <>
       <Header />
